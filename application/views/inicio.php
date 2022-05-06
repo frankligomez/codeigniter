254f0c1225193acb8f12b2e5bc1e96a452df57ba
frankligomez <f.gomez@misena.edu.co>
2022-05-06 12:45:12 -0500

    20220506_Frankli

---
+++ application/views/inicio.php
@@ -0,0 +1,22 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Bienvenido CodeIgniter</title>
+
+	
+</head>
+<body>
+
+<div id="container">
+	<h1>Bienvenido CodeIgniter</h1>
+
+	
+
+
+</div>
+
+</body>
+</html>
